833013f11334772f21cceb219af02f75a96b0f8d
LucasVaurie <lucas.vaurie91@gmail.com>
2020-10-27 14:42:50 +0100

    NumberPaths.java - V2.0 - Avec cache
    Algorithme finalisé et fonctionnel pour les 4 tests sur CodinGame.
    Avec système de cache.

--- NumberPaths.java
+++ NumberPaths.java
@@ -3,8 +3,16 @@ import java.io.*;
 import java.math.*;
 
 class NumberPaths {
-
     public static int calculChemins(int x, int y, int rows, int cols, int[][] array) {
+        int[][] cache = new int[rows+1][cols+1];
+        for (int i = 0 ; i <= rows ; i++) {
+            for (int j = 0 ; j <= cols ; j++)
+                cache[i][j] = -1;
+        }
+        return calculChemins(x, y, rows, cols, array, cache);
+    }
+
+    public static int calculChemins(int x, int y, int rows, int cols, int[][] array, int[][] cache) {
         // Cas où on sort des limites du tableau
         if (x == cols || y == rows)
             return 0;
@@ -14,9 +22,10 @@ class NumberPaths {
         // Cas où on est arrivé à (N-1,M-1)
         if (x == cols-1 && y == rows-1)
             return 1;
-        else return calculChemins(x, y+1, rows, cols, array) + calculChemins(x+1, y, rows, cols, array);
-
-    }
+        if (cache[x][y] == -1)
+            cache[x][y] = calculChemins(x, y+1, rows, cols, array, cache) + calculChemins(x+1, y, rows, cols, array, cache);
+        return cache[x][y];
+    } 
     
     public static void main(String args[]) {
         Scanner in = new Scanner(System.in);
