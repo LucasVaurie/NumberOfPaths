a5740b169ad62d6bc3dc9d0f3b8e2c3e307528d6
LucasVaurie <lucas.vaurie91@gmail.com>
2020-10-27 15:01:25 +0100

    NumberPaths.java - V2.1
    Ajout de détails (fusion de 2 'if' en 1, fermeture du 'Scanner') et ajout de Javadoc.

--- NumberPaths.java
+++ NumberPaths.java
@@ -1,32 +1,53 @@
-import java.util.*;
-import java.io.*;
-import java.math.*;
-
 class NumberPaths {
+    /**
+    * Initialise un tableau 'cache' à double entrée et
+    * appelle la fonction surchargée du même nom
+    *
+    * @param x
+    *     Ligne (row) de départ de l'analyse, en int
+    * @param y
+    *     Colonne (col) de départ de l'analyse, en int
+    * @param rows
+    *     Nombre de lignes du tableau, en int
+    * @param cols
+    *     Nombre de colonnes du tableau, en int
+    * @param array
+    *     Tableau double entrée représentant la carte (valeurs 0 ou 1), en int
+    * @return Le nombre de chemins possibles entre deux points (via appel de la fonction surchargée)
+    */
     public static int calculChemins(int x, int y, int rows, int cols, int[][] array) {
         int[][] cache = new int[rows+1][cols+1];
+        // initialisation du cache
         for (int i = 0 ; i <= rows ; i++) {
             for (int j = 0 ; j <= cols ; j++)
                 cache[i][j] = -1;
         }
         return calculChemins(x, y, rows, cols, array, cache);
     }
-
+    /**
+    * Calcule le nombre de chemins entre deux points, le départ (x, y) et l'arrivée (rows-1, cols-1)
+    * appelle la fonction surchargée du même nom
+    *
+    * @param cache
+    *     Tableau double entrée représentant le cache, avec valeurs déjà calculées ou non
+    * @return Le nombre de chemins possibles entre deux points (via appel de la fonction surchargée)
+    */
     public static int calculChemins(int x, int y, int rows, int cols, int[][] array, int[][] cache) {
-        // Cas où on sort des limites du tableau
-        if (x == cols || y == rows)
-            return 0;
-        // Cas où on rencontre une case interdite de valeur 1
-        if (array[x][y] == 1)
+        // Cas où on sort des limites du tableau OU Cas où on rencontre une case interdite de valeur 1
+        if ((x == cols || y == rows) || (array[x][y] == 1))
             return 0;
         // Cas où on est arrivé à (N-1,M-1)
         if (x == cols-1 && y == rows-1)
             return 1;
+        // Cas où donnée déjà dans le cache
         if (cache[x][y] == -1)
             cache[x][y] = calculChemins(x, y+1, rows, cols, array, cache) + calculChemins(x+1, y, rows, cols, array, cache);
         return cache[x][y];
     } 
-    
+
+    /**
+    * Fonction principale main du programme
+    */
     public static void main(String args[]) {
         Scanner in = new Scanner(System.in);
         int M = in.nextInt();
@@ -41,12 +62,9 @@ class NumberPaths {
             String ROW = in.nextLine();
             for (int j = 0 ; j < N ; j++) {
                 tab[i][j] = (int)(ROW.charAt(j)-'0');
-                //System.err.println(tab[i][j]);
             }
-            //System.err.println(ROW);
         }
-
-        //System.out.println("answer");
         System.out.println(calculChemins(0, 0, M, N, tab));
+        in.close();
     }
 }
